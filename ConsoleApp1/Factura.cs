2d548c2e27466a4ec0e6f7a961cda79ba2f795e0
Cupsuki001 <124387400+Cupsuki001@users.noreply.github.com>
2024-01-31 17:37:55 -0600

    Add files via upload

---
+++ ConsoleApp1/Factura.cs
@@ -0,0 +1,47 @@
+﻿using System;
+/*using System.Collections.Generic;*/
+/*using System.Linq;*/
+using System.Text;
+/*using System.Threading.Tasks;*/
+
+namespace ConsoleApp1 
+{
+   public class Factura
+    {
+        
+        double[,] Fact = new double[40,5];
+
+        public Factura() { }
+
+        public Factura(double[,] Fact)
+        {
+            this.Fact = Fact;
+        }
+
+        public void setFact(double[,] Fact)
+        {
+            this.Fact = Fact;
+        }
+
+        public int validar(String Mensaje)
+        {
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.Write(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+                    Console.WriteLine("No se aceptan letras", error.Message);
+                }
+            } while (v == 0);
+            return num;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,116 @@
+﻿using ConsoleApp1;
+public class SimFact
+{
+    public static void Main(String[] args)
+    {
+        Factura f = new Factura();
+        double[,] fa = new double[40, 5];
+        int alm, fila, col, ap = 0, rp = 0 , sal;
+        do
+        {
+
+
+            do
+            {
+                ap = 0;
+                rp = 0;
+                Console.WriteLine("-------------------------------------------------");
+                alm = f.validar("Digite la cantidad de estudiantes a evaluar:  ");
+                Console.WriteLine("///////////////////////////////////////////////");
+
+            } while (alm <= 0 || alm > 40);
+
+            for (fila = 0; fila < alm; fila++)
+            {
+
+                Console.WriteLine("Ingrese las Notas del estudiante #" + (1 + fila));
+
+                do
+                {
+
+                    fa[fila, 0] = f.validar("Nota 1 :");
+                    f.setFact(fa);
+                } while (fa[fila, 0] < 0 || fa[fila, 0] > 25);
+
+                do
+                {
+
+                    fa[fila, 1] = f.validar("Nota 2 :");
+                    f.setFact(fa);
+                } while (fa[fila, 0] < 0 || fa[fila, 0] > 25);
+
+                do
+                {
+
+                    fa[fila, 2] = f.validar("Nota 3 :");
+                    f.setFact(fa);
+                } while (fa[fila, 2] < 0 || fa[fila, 2] > 25);
+
+                do
+                {
+
+                    fa[fila, 3] = f.validar("Nota 4 :");
+                    f.setFact(fa);
+                } while (fa[fila, 3] < 0 || fa[fila, 3] > 25);
+
+                Console.WriteLine("------------------------------------//");
+
+
+                fa[fila, 4] = fa[fila, 0] + fa[fila, 1] + fa[fila, 2] + fa[fila, 3];
+
+            }
+            Console.WriteLine("-------------------RESULTADOS--------------------");
+            for (fila = 0; fila < alm; fila++)
+            {
+                for (col = 0; col < 5; col++)
+                {
+
+                    if (fa[fila, 4] >= 60)
+                    {
+                        Console.Write("\t" + fa[fila, col]);
+
+                    }
+                    else
+                    {
+                        Console.Write("\t" + fa[fila, col]);
+
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------------------------------------------------");
+            for (fila = 0; fila < alm; fila++)
+            {
+                if (fa[fila, 4] >= 60)
+                {
+                    Console.WriteLine("\t" + "Estudiante #" + (1 + fila) + " Aprobo" + "  Nota: " + fa[fila, 4]);
+                    ap++;
+
+                }
+                else
+                {
+                    Console.WriteLine("\t" + "Estudiante #" + (1 + fila) + " Reprobo" + " Nota: " + fa[fila, 4]);
+                    rp++;
+
+                }
+            }
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("\t" + "TOTAL Ingresados: " + alm);
+            Console.WriteLine("\t" + "Aprobados: " + ap);
+            Console.WriteLine("\t" + "Reprobados: " + rp);
+            Console.WriteLine("-------------------------------------------------");
+            do
+            {
+                sal = f.validar("Desea seguir en el programa? 1.Si 2.No" + "\t");
+
+            } while (sal <= 0 || sal > 2);
+
+        } while (sal == 1);
+        Console.WriteLine("Buen dia :)");
+        Console.ReadKey();
+    
+    }   
+    
+    
+}
